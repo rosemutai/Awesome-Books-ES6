0c14c0104e5d2ff6bc3ee94d0741c51a7897791b
rosemutai <chepngetichrose2030@gmail.com>
2023-03-27 13:32:46 +0300

    Add Javascript entry point

---
+++ index.js
@@ -0,0 +1,12 @@
+import AllBooks from './modules/AllBooks.js';
+import displayCurrentDate from './modules/Navbar.js';
+
+const books = new AllBooks();
+
+const form = document.getElementById('add-book-form');
+form.addEventListener('submit', (e) => {
+  e.preventDefault();
+  books.formAddBook();
+});
+
+document.addEventListener('DOMContentLoaded', displayCurrentDate);
